65c68a1a891c6cd8c2878e6ed5788b89ed3bd041
nabilaa2308 <nabilarahmawati43@smk.belajar.id>
2022-09-22 15:13:32 +0700

    commit

--- app/Http/Controllers/DashboarController.php
+++ app/Http/Controllers/DashboarController.php
@@ -2,13 +2,24 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Tag;
+use App\Models\Post;
+use App\Models\Kategori;
 use Illuminate\Http\Request;
 
 class DashboarController extends Controller
 {
     public function index()
     {
-        return view('layouts.dashboard');
+        $posts = Post::count();
+        $tags = Tag::count();
+        $kategoris = Kategori::count();
+        return view('dashboard', [
+            'title' => 'Dashboard',
+            'posts' => $posts,
+            'tags' => $tags,
+            'kategoris' => $kategoris
+        ]);
     }
 
    
