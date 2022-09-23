0ce4556f8a7886ee55d888bce434f2015893d297
nabilaa2308 <nabilarahmawati43@smk.belajar.id>
2022-09-23 13:52:07 +0700

    commit

--- resources/views/kategori.blade.php
+++ resources/views/kategori.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.main')
 
 @section('title')
-Ocoding Bog | {{ $title }}
+    Ocoding Bog | {{ $title }}
 @endsection
 
 @section('container')
@@ -12,31 +12,33 @@ Ocoding Bog | {{ $title }}
         <h1 class="text-white mt-4 mb-4 text-center">Kategori</h1>
     </div>
     <br>
-     
-     <!-- List category -->
-     <div class="container border py-5">
-     <div class="row flex-row">
-        @forelse ($kategoris as $dtkategori)
-            <!-- true -->
-            <div class="col-lg-4 col-md-6 pb-4">
-               <a class="courses-list-item position-relative d-block overflow-hidden mb-2" href="{{ route('post-kategori', ['slug' => $dtkategori->slug]) }}">
-                   <img class="img-fluid rounded" src="{{ asset('storage/..' . $dtkategori->thumbnail) }}" alt="kategori">
-                   <div class="courses-text">
-                      <div class="border-top w-100 mt-3">
-                          <div class="d-flex justify-content-between p-4 text-center">
-                       <h4 class="text-center text-white px-3">{{ $dtkategori->name }}</h4>
-                           </div>
-                       </div>
-                   </div>
-               </a>
-           </div>
-             
-        @empty
-            <!-- false -->
-        <h3 class="text-center">
-            Belum Ada Data
-         </h3>
-        @endforelse
-     </div>     
+
+    <!-- List category -->
+    <div class="container border py-5">
+        <div class="row flex-row">
+            @forelse ($kategoris as $dtkategori)
+                <!-- true -->
+                <div class="col-lg-4 col-md-6 pb-4">
+                    <a class="courses-list-item position-relative d-block overflow-hidden mb-2"
+                        href="{{ route('post-kategori', ['slug' => $dtkategori->slug]) }}">
+                        <img class="img-fluid rounded" src="{{ asset('storage/..' . $dtkategori->thumbnail) }}"
+                            alt="kategori">
+                        <div class="courses-text">
+                            <div class="border-top w-100 mt-3">
+                                <div class="d-flex justify-content-between p-4 text-center">
+                                    <h4 class="text-center text-white px-3">{{ $dtkategori->name }}</h4>
+                                </div>
+                            </div>
+                        </div>
+                    </a>
+                </div>
+
+            @empty
+                <!-- false -->
+                <h3 class="text-center">
+                    Belum Ada Data
+                </h3>
+            @endforelse
+        </div>
     </div>
 @endsection
